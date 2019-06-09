6e34aa4af4aaa52cce25d4435b183927ff28af0d
Karim <abdounikarim@gmail.com>
2019-06-09 12:27:37 +0200

    Requiring SweetAlert2

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -2,8 +2,9 @@
 
 const Helper = require('./RepLogAppHelper');
 const $ = require('jquery');
+const swal = require('sweetalert2');
 
-(function(window, Routing, swal) {
+(function(window, Routing) {
 
     let HelperInstances = new WeakMap();
 
@@ -214,4 +215,4 @@ const $ = require('jquery');
 `;
 
     window.RepLogApp = RepLogApp;
-})(window, Routing, swal);
+})(window, Routing);
