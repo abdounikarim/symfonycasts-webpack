3eb46398e8d4c7c07cb5e144ba1266dfd34fdacf
Karim <abdounikarim@gmail.com>
2019-06-15 01:32:17 +0200

    babel-loader Options & Polyfill

--- web/assets/js/layout.js
+++ web/assets/js/layout.js
@@ -3,6 +3,9 @@
 const $ = require ('jquery');
 require ('bootstrap');
 
+// make sure the polyfill library is loaded in this main entry
+require ('babel-polyfill');
+
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,12 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
+                exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader'
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true
+                    }
                 }
             }
         ]
