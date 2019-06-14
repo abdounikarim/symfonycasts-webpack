5af21aae26eb2bacb8d29be95fd1cc03b0a724d2
Karim <abdounikarim@gmail.com>
2019-06-15 01:55:46 +0200

    Bootstrap CSS & Fonts

--- web/assets/js/layout.js
+++ web/assets/js/layout.js
@@ -3,6 +3,7 @@
 const $ = require ('jquery');
 require ('bootstrap');
 
+require('bootstrap/dist/css/bootstrap.css');
 require('../css/main.css');
 
 // make sure the polyfill library is loaded in this main entry
