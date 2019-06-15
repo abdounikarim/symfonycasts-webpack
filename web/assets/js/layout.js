8fba6695ca333c761edf065b3ad7caea8cd33708
Karim <abdounikarim@gmail.com>
2019-06-15 02:00:28 +0200

    Font Awesome & file-loader [hash]

--- web/assets/js/layout.js
+++ web/assets/js/layout.js
@@ -4,6 +4,7 @@ const $ = require ('jquery');
 require ('bootstrap');
 
 require('bootstrap/dist/css/bootstrap.css');
+require('font-awesome/css/font-awesome.css');
 require('../css/main.css');
 
 // make sure the polyfill library is loaded in this main entry
--- webpack.config.js
+++ webpack.config.js
@@ -33,15 +33,21 @@ module.exports = {
             },
             {
                 test: /\.(png|jpg|jpeg|gif|ico|svg)$/,
-                use: [
-                    'file-loader'
-                ]
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name]-[hash:6].[ext]'
+                    }
+                }
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name]-[hash:6].[ext]'
+                    }
+                }
             }
         ]
     },
