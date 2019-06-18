15a3fdf7b926ec6bc0229cf0edf378a95792c08f
Karim <abdounikarim@gmail.com>
2019-06-18 18:54:32 +0200

    Passing Server Data to JS

--- assets/js/rep_log.js
+++ assets/js/rep_log.js
@@ -6,5 +6,5 @@ global.$ = $;
 
 $(document).ready(function() {
     var $wrapper = $('.js-rep-log-table');
-    var repLogApp = new RepLogApp($wrapper);
+    var repLogApp = new RepLogApp($wrapper, $wrapper.data('rep-logs'));
 });
